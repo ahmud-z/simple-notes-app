8eb2c8a16ecef79aac1b388d7e50a3d8dba76c6c
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-07-15 09:15:56 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const createBtn = document.getElementById('createBtn');
+const notesContainer = document.getElementById('notesContainer');
+let notesClass = document.querySelectorAll('.note-box');
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+createBtn.addEventListener("click", function () {
+    let note = document.createElement('p');
+    let img = document.createElement("img");
+    note.setAttribute("contenteditable", "true");
+    note.setAttribute.Placeholder = "Hello"
+    note.className = "note-box"
+    img.src = "images/delete.png"
+    notesContainer.appendChild(note).appendChild(img);
+})
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === "P") {
+        notesClass = document.querySelectorAll(".note-box");
+        notesClass.forEach(item => {
+            item.onkeyup = function () {
+                updateStorage();
+            }
+        });
+    }
+});
+
+
